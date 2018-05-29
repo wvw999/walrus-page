9241e70fb9c523495b9fb18eaf7cfac7e1f1263c
Kelli Brooke and Will Swanson <will@>
2018-05-29 15:28:02 -0700

    built walrus hiding page which hides four walruses with amazing skill.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,20 @@ $(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $("#fade").fadeIn();
+
+  $("#fade").click(function(){
+    $("#fade").fadeOut();
+  });
+
+  $("#slide").slideDown();
+
+  $("#slide").click(function(){
+    $("#slide").slideUp();
+  });
+
+  $("#button").click(function(){
+    $("#slide-toggle").slideToggle();
+  })
 });
